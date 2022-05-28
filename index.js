335cb4df3439b1b81d8cd60ec044e75070379296
tikibeni <benjamin.blinnikka@hotmail.com>
2022-05-29 02:23:33 +0300

    Tehtävä 13.3.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const { Sequelize } = require('sequelize')
+const { Sequelize, QueryTypes } = require('sequelize')
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialectOptions: {
@@ -8,12 +8,13 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
             rejectUnauthorized: false
         }
     },
-})
+});
 
 const main = async () => {
     try {
         await sequelize.authenticate()
-        console.log('Connection has been established successfully.')
+        const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
+        blogs.forEach(blog => console.log(`${blog.author}: '${blog.title}', ${blog.likes} likes`))
         sequelize.close()
     } catch (error) {
         console.error('Unable to connect to the database:', error)
