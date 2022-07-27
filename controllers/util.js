b57e7c52325cb99f9d6b2b3bfa337a67e0e31ab3
tikibeni <benjamin.blinnikka@hotmail.com>
2022-07-27 17:28:58 +0300

    Tehtävä 13.9.

--- controllers/util.js
+++ controllers/util.js
@@ -5,7 +5,10 @@ const errorHandler = (error, req, res, next) => {
     if (error.name === 'CastError') {
         return res.status(400).send({ error: 'malformatted id' })
     }
-    if (['ValidationError', 'SequelizeDatabaseError'].includes(error.name)) {
+    if (['ValidationError', 'SequelizeValidationError', 'SequelizeDatabaseError'].includes(error.name)) {
+        if (error.name === 'SequelizeValidationError') {
+            return res.status(400).json({ message: `Toimintoon liittyvä validointivirhe - korjaa arvojen muodot. Lisäinfoa: ${error.message}` })
+        }
         return res.status(400).json({ message: error.message })
     }
 
